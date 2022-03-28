d5c1c72709baf6f3fcffea4d4abb470d922b49df
monjourur <md899042@gmail.com>
2022-03-28 14:43:50 +0600

    update

--- app/src/main/java/com/example/printing_project/MainActivity.java
+++ app/src/main/java/com/example/printing_project/MainActivity.java
@@ -2,14 +2,114 @@ package com.example.printing_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_create_pdf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_create_pdf = findViewById(R.id.PdfButton);
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        btn_create_pdf.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                createPDFFile(Common.getAppPath(MainActivity.this)+"test_pdf.pdf");
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                })
+                .check();
+    }
+
+    private void createPDFFile(String path) {
+
+        if(new File(path).exists())
+            new  File(path).delete();
+        try {
+            Document document = new Document();
+            PdfWriter.getInstance(document,new FileOutputStream( path));
+            document.open();
+            document.setPageSize(PageSize.A4);
+            document.addCreationDate();
+            document.addAuthor("fahim");
+            document.addCreator("Famin");
+
+            BaseColor colorAccent = new  BaseColor(0,153,204,255);
+            float fontSize = 20.0f;
+            float valueFontSize = 26.0f;
+
+            BaseFont fontName= BaseFont.createFont("assets/fonts/brandon_medium.otf","UTF-8",BaseFont.EMBEDDED);
+
+            Font titleFont = new Font( fontName,36.0f,Font.NORMAL,BaseColor.BLACK);
+            addNewItem(document,"Order Details", Element.ALIGN_CENTER,titleFont);
+
+
+
+
+
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void addNewItem(Document document, String text, int align, Font font) throws DocumentException {
+
+        Chunk chunk = new Chunk(text,font);
+
+        Paragraph paragraph = new Paragraph(chunk);
+        paragraph.setAlignment(align);
+        document.add(paragraph);
     }
 }
